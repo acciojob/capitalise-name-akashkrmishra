2683dd5fb267b8b676a3826bb92eed0f2cb966b2
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-24 07:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function ConvertTOUpperCase(){
-	var inputfield = document.getElementById('fname');
+	const inputfield = document.getElementById('fname');
 	inputfield.value = inputfield.value.toUpperCase();
 }
